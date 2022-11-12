6e6d6905b656d1063e5bd748913448b3ea59de7a
Carlos Bronze <170220+bronze@users.noreply.github.com>
2022-11-12 19:58:08 -0300

    integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,16 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
+import UnoCSS from 'unocss/astro';
 
 // https://astro.build/config
-export default defineConfig({});
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
+import prefetch from "@astrojs/prefetch";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [UnoCSS(), sitemap(), image(), prefetch()]
+});
