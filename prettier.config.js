fcf0d99a1e92ca79efe02940cbdbeebc3413a523
Carlos Bronze <170220+bronze@users.noreply.github.com>
2023-08-23 22:55:52 -0300

    prettier

---
+++ prettier.config.js
@@ -0,0 +1,25 @@
+module.exports={
+  tailwindConfig: './tailwind.config.js', // ⚠️ Set it so you're sure to call up the Tailwind config in the right place.
+  useTabs: false,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  bracketSpacing: false,
+  bracketSameLine: true,
+  trailingComma: 'none',
+  quoteProps: 'consistent',
+  plugins: [
+    'prettier-plugin-organize-attributes',
+    'prettier-plugin-tailwindcss',  // ⚠️ Order is very important place Tailwind prettier plugin at the end
+  ],
+  overrides: [ // ⚠️ If you are using edge you need also set this to add support for edge
+    {
+      files: '*.njk',
+      options: {
+        parser: 'html',
+      },
+    },
+  ],
+};
