8e4933edf70fa2aa3ab24c2bce556ce45add2417
Carlos Bronze <170220+bronze@users.noreply.github.com>
2023-08-23 23:18:52 -0300

    prettier njk

--- prettier.config.js
+++ prettier.config.js
@@ -15,11 +15,6 @@ module.exports={
     'prettier-plugin-tailwindcss',  // ⚠️ Order is very important place Tailwind prettier plugin at the end
   ],
   overrides: [ // ⚠️ If you are using edge you need also set this to add support for edge
-    {
-      files: '*.njk',
-      options: {
-        parser: 'html',
-      },
-    },
+    {files: '*.njk', options: {parser: 'html', }, },
   ],
 };
